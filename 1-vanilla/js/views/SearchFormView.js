3b4ea013d8a4968aa61fa210a82ee79ae8926d51
GoJiMin <gojimin3095@gmail.com>
2024-10-30 18:29:00 +0900

    검색 폼 구현: 검색어를 입력하면 X 버튼이 보이고 검색어를 삭제하면 X 버튼을 숨긴다.
    - 입력을 감지하기 위해 input 요소를 찾아 SearchFormView 클래스에 정의한다.
    - bindEvent 함수를 생성해 target 요소로 input 요소를 전달해 keyup 이벤트를 감지하도록 설정한다.
    - keyup 이벤트를 감지해 handleKeyup 함수를 실행하며 input 요소의 value 값이 0 이상이면 X 버튼을 보인다.

--- 1-vanilla/js/views/SearchFormView.js
+++ 1-vanilla/js/views/SearchFormView.js
@@ -1,4 +1,4 @@
-import { qs } from "../helpers.js";
+import { on, qs } from "../helpers.js";
 import View from "./View.js";
 
 export default class SearchFormView extends View {
@@ -6,11 +6,24 @@ export default class SearchFormView extends View {
     super(qs("#search-form-view"));
 
     this.resetElement = qs("[type=reset]", this.element);
+    this.inputElement = qs("[type=text]", this.element);
 
     this.showResetButton(false);
+
+    this.bindEvent();
   }
 
   showResetButton(visible = true) {
     this.resetElement.style.display = visible ? "block" : "none";
+  }
+
+  bindEvent() {
+    on(this.inputElement, "keyup", () => this.handleKeyup());
+  }
+
+  handleKeyup() {
+    const { value } = this.inputElement;
+
+    this.showResetButton(value.length > 0);
   }
 }
